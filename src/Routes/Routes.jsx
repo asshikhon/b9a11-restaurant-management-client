8859fa0fae1945c4674bec968dcb646d18e4bf72
asshikhon <asshikhon@gmail.com>
2024-05-11 19:00:06 +0600

    create register page with functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import ContactUs from "../Pages/ContactUs/ContactUs";
 import ErrorPage from "../Errorpage/Errorpage";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,16 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+path: "/register",
+element: <Register></Register>
+
+      },
+//       {
+// path: "/login",
+// element: 
+
+//       },
       {
         path: "/contact",
         element: <ContactUs></ContactUs>,
